d2e39431b5cd3337a5e68c6d04fdd06d2651e0d3
Aaron van den Berg <a.vdberg98@gmail.com>
2023-03-29 21:25:08 +0200

    Changed some config for Expo EAS

--- apps/app/app.config.js
+++ apps/app/app.config.js
@@ -3,9 +3,16 @@ module.exports = {
   web: {
     bundler: 'metro',
   },
-  name: 'drinking-game-app',
+  name: 'Drinking Game App',
   slug: 'drinking-game-app',
   extra: {
     colyseusApiUrl: "wss://drinking-game-app-production.up.railway.app",
+    eas: {
+      projectId: "dbcb5f8c-6dac-4156-ab15-fd91f8338317"
+    }
   },
+  android: {
+    package: "com.aaronvandenberg.drinkinggameapp",
+    versionCode: 1
+  }
 };
